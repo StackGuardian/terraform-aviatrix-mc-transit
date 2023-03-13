6a880e4d567cc2bae72d51e15a4c2d787c8b2089
Johannes Scheuerer <joscheuerer@gmail.com>
2023-03-13 14:15:44 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -7,3 +7,13 @@ terraform {
   }
   required_version = ">= 1.1.1"
 }
+
+variable "aviatrix_controller_ip" {}
+variable "aviatrix_username" {}
+variable "aviatrix_password" {}
+
+provider "aviatrix" {
+  controller_ip           = var.aviatrix_controller_ip
+  username                = var.aviatrix_username
+  password                = var.aviatrix_password
+}
